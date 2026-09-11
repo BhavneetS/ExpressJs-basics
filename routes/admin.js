5d68a2c0fb05db258d34b2ce24d68c7a159f818d
BhavneetS <bhavneet.singh@hotmail.com>
2026-09-12 00:18:09 +0530

    Send file as response and resolve path of file using __dirname and path module

--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,13 @@
 const router = require('express').Router();
+const path = require('path');
 
 router.get('/add-product', (req, res) => {
-   return res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
+    /* 
+        While sending a file, we need to conact the path in this way as the base route will be the server route and not the application route. 
+        __dirname will give the current directory route So we need to go back to the application route and then go to the views folder and then get the file.
+        Also, do not concat the path using string concatenation as it will not work in all OS. Use path.join() to join the paths.
+    */
+   return res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'))
 })
 
 router.post('/product', (req, res) => {
