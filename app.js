6d10fdfa225b1619b8763def58d6d11874649910
BhavneetS <bhavneet.singh@hotmail.com>
2026-09-11 18:32:02 +0530

    handling error on non-existant routes

--- app.js
+++ app.js
@@ -15,11 +15,14 @@ app.get('/favicon.ico', (req, res) => {
     parse data so that it is available in req.body.
 */
 app.use(express.urlencoded({ extended: true }));
-app.use(adminRoutes);
+
+
+app.use(adminRoutes); /* we can also use app.use('/admin' , adminRoutes) to filter routes as /admin/... */
 app.use(shopRoutes);
 
-app.use('/', (req, res, next) => {
-   return res.send('<h1>Hello from Express!</h1>');
+
+app.use((req, res, next) => {
+   return res.status(404).send('<h1>Page not found</h1>');
 })
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -4,6 +4,12 @@ router.use('/users', (req, res) => {
    return res.send('<h1>Hello from users!</h1>')
 })
 
+router.get('/', (req, resp, next) => {
+    console.log(req.url);
+    resp.send('<h1>Hello from Express!</h1>');
+})
+
+
 /* 
     The data is not available here if not using body-parser or express.json() middleware.
 */
