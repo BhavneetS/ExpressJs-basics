f8bd8b2184db8960e3f38d0f3765d6fbc040f64d
BhavneetS <bhavneet.singh@hotmail.com>
2026-09-15 00:18:12 +0530

    Dynamic data bining using Handlebars template rendering engine

--- app.js
+++ app.js
@@ -2,12 +2,28 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const handlebars = require('express-handlebars');
 
 
 const rootDir = require('./utils/pathUtil'); 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop')
 
+
+
+/* 
+    Since handlebars is not a built-in template engine in express, we need to install it using npm install express-handlebars and then we need to register it as a template engine using app.engine() method.
+    The first argument is the name of the template engine and the second argument is the template engine itself.
+    The third argument is an object which contains the configuration options for the template engine.
+*/
+/* 
+    views/layouts/ is the default location for the layouts folder. 
+    So we don't need to specify the layoutsDir option in the express-handlebars engine configuration. But if we want to change the location of the layouts folder, we can use the layoutsDir option to specify the location of the layouts folder.
+    extname needs to be explicitly defined for handlebars layout engine as it does not have a default extension. So we need to specify the extension of the layout files using the extname option.
+*/
+app.engine('hbs', handlebars.engine({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'}));
+app.set('view engine', 'hbs');
+// app.set('views', path.join(__dirname, 'views'));
 /* 
     app.set() is used to set global variables in the application. 
     It is used to set the view engine and the views folder path.
@@ -19,7 +35,7 @@ const shopRoutes = require('./routes/shop')
 /* 
     pug ships with inherent support for express. So we don't need to install any additional package to use pug with express.
 */
-app.set('view engine', 'pug');
+// app.set('view engine', 'pug');
 /* 
     app.get only works for get calls. Similarly app.post only works for post calls.
 */
--- routes/shop.js
+++ routes/shop.js
@@ -17,7 +17,15 @@ router.get('/', (req, resp, next) => {
         As we need to render the view using pug, we need to use resp.render() method.
         The first argument is the name of the view file without the extension and the second argument is an object which contains the data to be passed to the view.
     */
-    resp.render(path.join('shop'), {prods: products, docTitle: 'Shop', path:'/'})
+    resp.render(path.join('shop'), {
+        prods: products, 
+        docTitle: 'Shop', 
+        path:'/',
+        isAddProductActive: false,
+        isShopActive: true,
+        /* Since hanlebars is lightweight and cannot evaluate conditions, we need to pass the condition as a property */
+        hasProducts: products.length > 0
+    })
 })
 
 
